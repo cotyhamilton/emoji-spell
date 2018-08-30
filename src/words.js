f812765e42778aec9078d13d8933c4fb03019d95
Coty Hamilton <cotyhamilton@gmail.com>
2018-08-30 14:55:06 -0500

    list of words and emojis

---
+++ src/words.js
@@ -0,0 +1,14 @@
+const words = {
+    'alien': '👽',
+    'baby': '👶',
+    'car': '🚗',
+    'cat': '🐈',
+    'dog': '🐕',
+    'ghost': '👻',
+    'glasses': '👓',
+    'octopus': '🐙',
+    'robot': '🤖',
+    'shirt': '👕'
+}
+
+export default words;
