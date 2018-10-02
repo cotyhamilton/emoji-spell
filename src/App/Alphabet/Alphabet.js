1b999a456d99a6bb2d755adcef8351203488826f
Coty Hamilton <cotyhamilton@gmail.com>
2018-10-02 16:05:55 -0500

    add function to choose from random letters and styled new letter grid

--- src/App/Alphabet/Alphabet.js
+++ src/App/Alphabet/Alphabet.js
@@ -2,6 +2,23 @@ import React, { Component } from 'react';
 import './Alphabet.css';
 
 class Alphabet extends Component {
+    constructor(props) {
+        super(props);
+        let pool = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+        this.letterList = this.props.goal.split('');
+        this.letterList = this.letterList.filter(function(e, i, l) {
+            return i === l.indexOf(e);
+        });
+        pool = pool.filter((e) => !this.letterList.includes(e));
+        while(this.letterList.length < 16) {
+            this.letterList.push(pool.pop([Math.floor(Math.random()*pool.length)]));
+        }
+        for(let i = 0; i < 16; i++) {
+            let r = Math.floor(Math.random()*16);
+            this.letterList[i] = [this.letterList[r], this.letterList[r] = this.letterList[i]][0];
+        }
+    }
+
     update = (l) => {
         this.props.update(l);
     }
@@ -9,7 +26,7 @@ class Alphabet extends Component {
     render () {
         return(
             <div className="alphabet">
-                {['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'].map((l,i) =>
+                {this.letterList.map((l,i) =>
                     <button className="letter" onClick={(e) => this.update(l, e)} letter={l} key={i}>{l}</button>
                 )}
             </div>
