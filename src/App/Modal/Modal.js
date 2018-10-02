c168b3e96e36d7399ec4f937452e9adf001b444e
Coty Hamilton <cotyhamilton@gmail.com>
2018-10-02 16:04:32 -0500

    styled modal

--- src/App/Modal/Modal.js
+++ src/App/Modal/Modal.js
@@ -7,19 +7,9 @@ class Modal extends Component {
             <div className="modal">
                 {this.props.win === true
                     ?
-                        <div className="modal-content">
-                            <h1><i className="em-svg em-tada wiggle"></i></h1>
-                            <div className="modal-text">
-                                <h3 className="restart" onClick={this.props.close}>NEW</h3>
-                            </div>
-                        </div>
+                        <h1 onClick={this.props.close}><i className="em-svg em-tada wiggle"></i></h1>
                     :
-                        <div className="modal-content">
-                            <h1><i className="em-svg em-flushed wiggle"></i></h1>
-                            <div className="modal-text">
-                                <h3 className="restart" onClick={this.props.close}>TRY AGAIN</h3>
-                            </div>
-                        </div>}
+                        <h1 onClick={this.props.close}><i className="em-svg em-flushed wiggle"></i></h1>}
             </div>   
         )
     }
